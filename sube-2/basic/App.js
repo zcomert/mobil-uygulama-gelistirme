ba740593e0081cf8090971818be350bdb9263047
zcomert <zcomert@samsun.edu.tr>
2024-03-14 15:22:52 +0300

    map, filtering, Pressable have been added.

--- sube-2/basic/App.js
+++ sube-2/basic/App.js
@@ -2,10 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import TimeWidget from './src/basic/TimeWidget'
 import Counter from './src/basic/Counter'
+import BookSeries from './src/components/bookSeries/BookSeries'
 
 const App = () => {
   return (
-   <Counter />
+   <BookSeries />
   )
 }
 
---
+++ sube-2/basic/src/components/bookSeries/colors.js
@@ -0,0 +1,7 @@
+const Colors = {
+    primary500: '#E26A05',
+    secondary500:'#FFF4E2',
+    white:'#fff'
+}
+
+export default Colors;
