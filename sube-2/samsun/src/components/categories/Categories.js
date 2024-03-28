8649d0a49d4112148d4bf5df23b64749ae55efe1
zcomert <zcomert@samsun.edu.tr>
2024-03-28 14:29:51 +0300

    CategoryHeader has been updated.

--- sube-2/samsun/src/components/categories/Categories.js
+++ sube-2/samsun/src/components/categories/Categories.js
@@ -1,29 +1,29 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
-import CategoryHeader from './CategoryHeader'
-import categories from './categoryData';
-import CategoryIcon from './CategoryIcon'
-const Categories = () =>
-{
-    const renderItem = ({item}) => (
-        <View>
-            <CategoryIcon 
-            icon={item?.icon}
-            name={item?.categoryName} />
-        </View>
-    )
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import CategoryHeader from "./CategoryHeader";
+import categories from "./categoryData";
+import CategoryIcon from "./CategoryIcon";
+const Categories = () => {
+  const renderItem = ({ item }) => (
+    <View>
+      <CategoryIcon icon={item?.icon} name={item?.categoryName} />
+    </View>
+  );
 
+  return (
+    <View>
+      <CategoryHeader name="Kategoriler" />
+     
+        <FlatList
+            horizontal={false}
+            numColumns={3}
+            keyExtractor={(item) => item.categoryId}
+            renderItem={renderItem}
+            data={categories}
+        />
+      
+    </View>
+  );
+};
 
-    return (
-        <View>
-            <CategoryHeader name="Kategoriler" />
-            <FlatList 
-                numColumns={3}
-                keyExtractor={(item) => item.categoryId }
-                renderItem={renderItem}
-                data={categories} />
-        </View>
-    )
-}
-
-export default Categories
+export default Categories;
--- sube-2/samsun/src/components/categories/CategoryIcon.js
+++ sube-2/samsun/src/components/categories/CategoryIcon.js
@@ -1,27 +1,43 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
+import Colors from "../../constants/colors";
 
 const CategoryIcon = ({ name, icon }) => {
   return (
-    <View style={styles.contanier}>
-      <View style={styles.icon}>
-        <FontAwesome5 name={icon} size={24} color="black" />
+    <View style={styles.outContainer}>
+      <View style={styles.contanier}>
+        <View style={styles.icon}>
+          <FontAwesome5 name={icon} size={24} color={Colors.white} />
+        </View>
       </View>
-      <View style={styles.text}>
-        <Text>{name}</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{name}</Text>
       </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
+  outContainer: {
+    justifyContent: "flex-start",
+    alignItems:'center',
+    // borderWidth:1,
+    width:75,
+    marginHorizontal:5,
+  },
   contanier: {
     padding: 5,
     margin: 5,
-    borderWidth: 1,
+    backgroundColor: Colors.primary500,
+    borderRadius:100
   },
   icon: {},
-  text: {},
+  textContainer: {
+    flexWrap:'wrap'
+  },
+  text: {
+    fontSize: 8,
+  },
 });
 
 export default CategoryIcon;
