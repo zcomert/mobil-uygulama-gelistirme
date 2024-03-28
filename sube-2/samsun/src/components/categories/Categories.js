fce7616748d7423cf3e7924e5e975edc086504be
zcomert <zcomert@samsun.edu.tr>
2024-03-28 13:27:53 +0300

    CategoryHeader has been updated.

--- sube-2/samsun/src/components/categories/Categories.js
+++ sube-2/samsun/src/components/categories/Categories.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import CategoryHeader from './CategoryHeader'
 const Categories = () =>
 {
     return (
         <View>
+            <CategoryHeader name="Kategoriler" />
             <Text>Categories</Text>
         </View>
     )
--- sube-2/samsun/src/components/categories/CategoryHeader.js
+++ sube-2/samsun/src/components/categories/CategoryHeader.js
@@ -1,13 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-const CategoryHeader = () =>
-{
-    return (
-        <View>
-            <Text>CategoryHeader</Text>
-        </View>
-    )
-}
+const CategoryHeader = ({ name }) => {
+  return (
+    <View>
+      <View>
+        <Text>{name}</Text>
+      </View>
+      <View>
+        <FontAwesome5 name="list" size={24} color="black" />
+      </View>
+    </View>
+  );
+};
 
-export default CategoryHeader
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default CategoryHeader;
