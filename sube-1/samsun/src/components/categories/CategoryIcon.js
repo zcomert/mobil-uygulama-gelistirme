0ee5a83184d0b0f98b2d5e935db1efec19968105
zcomert <zcomert@samsun.edu.tr>
2024-03-28 11:42:17 +0300

    CategoryIcon has been defined.

--- sube-1/samsun/src/components/categories/CategoryIcon.js
+++ sube-1/samsun/src/components/categories/CategoryIcon.js
@@ -1,15 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { FontAwesome5 } from '@expo/vector-icons';
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
+import Colors from "../../constants/colors";
 
-const CategoryIcon = ({icon,name}) =>
-{
-    return (
-        <View>
-            <Text>{name}</Text>
-            <FontAwesome5 name={icon} size={24} color="black" />
-        </View>
-    )
-}
+const CategoryIcon = ({ icon, name }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.item}>
+        <FontAwesome5 name={icon} size={24} color={Colors.white} />
+      </View>
+        <Text>{name}</Text>
+    </View>
+  );
+};
 
-export default CategoryIcon
+const styles = StyleSheet.create({
+  container: {
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  item: {
+        backgroundColor:Colors.primary500,
+        margin:8,
+        padding:8,
+        borderRadius:100, 
+  },
+});
+
+export default CategoryIcon;
