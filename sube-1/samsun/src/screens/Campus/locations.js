a99becec0ef953af3a66b41937af75fd2d052e6d
zcomert <zafer.comert@hotmail.com>
2024-04-20 22:41:23 +0300

    geolocation-start has been modified.

---
+++ sube-1/samsun/src/screens/Campus/locations.js
@@ -0,0 +1,52 @@
+const INITAL_REGION = {
+    latitude: 41,
+    longitude: 36,
+    latitudeDelta: 1,
+    longitudeDelta: 1
+}
+
+const SamsunUniversity = {
+
+    latitude: 41.5115768,
+    longitude: 36.1091823,
+    latitudeDelta: 0.03,
+    longitudeDelta: 0.03,
+}
+
+const markers = [
+    {
+        latitude: 41.2868071,
+        longitude: 36.3366578,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+        name: 'Rektörlük Şehir Ofisi'
+    },
+    {
+        latitude: 41.2318417,
+        longitude: 36.3673084,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+        name: 'Canik Yerleşkesi'
+    },
+    {
+        latitude: 41.5115768,
+        longitude: 36.1091823,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+        name: 'Ballıca Yerleşkesi'
+    },
+    {
+        latitude: 41.0788902,
+        longitude: 36.0440721,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+        name: 'Kavak Yerleşkesi'
+    }
+];
+
+export
+{
+    INITAL_REGION,
+    SamsunUniversity,
+    markers
+}
