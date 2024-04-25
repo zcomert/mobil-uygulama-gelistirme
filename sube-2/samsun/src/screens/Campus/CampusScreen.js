5cfb72a1fdaaa8ae6cd6b5a96e3a29ce674ac2c1
zcomert <zcomert@samsun.edu.tr>
2024-04-25 15:09:14 +0300

    MapView has been complated.

--- sube-2/samsun/src/screens/Campus/CampusScreen.js
+++ sube-2/samsun/src/screens/Campus/CampusScreen.js
@@ -1,33 +1,60 @@
 import { View, Text, StyleSheet } from "react-native";
-import React, { useLayoutEffect } from "react";
-import MapView from "react-native-maps";
-import { INITAL_REGION } from "./locations";
+import React, { useLayoutEffect, useRef } from "react";
+import MapView, { MapCallout, MapMarker } from "react-native-maps";
+import { INITAL_REGION, markers, SamsunUniversity } from "./locations";
 import { TouchableOpacity } from "react-native";
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5 } from "@expo/vector-icons";
 
-const CampusScreen = ({navigation}) => {
+const CampusScreen = ({ navigation }) => {
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight:() => (
-            <TouchableOpacity>
-                <FontAwesome5 
-                    name="map-marker-alt" 
-                    size={24} 
-                    color="black" />
-            </TouchableOpacity>
-            )
-        })
+    const handleRegionChange = (region) => {
+        console.log(region)
+    }
+
+  const mapRef = useRef();
+
+  const handlePress = () => {
+    mapRef.current.animateCamera(
+      {
+        center: SamsunUniversity,
+        zoom: 15,
+      },
+      {
+        duration: 5000,
+      }
+    );
+    //    .animateToRegion(INITAL_REGION)
+  };
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={handlePress}>
+          <FontAwesome5 name="map-marker-alt" size={24} color="black" />
+        </TouchableOpacity>
+      ),
     });
+  });
 
   const { container, map } = styles;
   return (
     <View style={container}>
-      <MapView 
-      showsUserLocation
-      showsMyLocationButton
-      initialRegion={INITAL_REGION}
-      style={map} />
+      <MapView
+        showsUserLocation
+        showsMyLocationButton
+        initialRegion={INITAL_REGION}
+        ref={mapRef}
+        style={map}
+        onRegionChange={(region) => handleRegionChange(region)}
+        // onRegionChangeComplete={(region) => handleRegionChange(region)}
+      >
+        {markers.map((item, index) => (
+          <MapMarker key={index} coordinate={item}>
+            <MapCallout>
+              <Text>{item?.name}</Text>
+            </MapCallout>
+          </MapMarker>
+        ))}
+      </MapView>
     </View>
   );
 };
