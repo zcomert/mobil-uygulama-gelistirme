37c32233f76850cf18e8a0ddfc09eedaf1f04696
zcomert <zcomert@samsun.edu.tr>
2024-04-25 14:11:07 +0300

    MapView has been added.

--- sube-2/samsun/src/screens/Campus/CampusScreen.js
+++ sube-2/samsun/src/screens/Campus/CampusScreen.js
@@ -1,13 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React, { useLayoutEffect } from "react";
+import MapView from "react-native-maps";
+import { INITAL_REGION } from "./locations";
 
-const CampusScreen = () =>
-{
-    return (
-        <View>
-            <Text>CampusScreen</Text>
-        </View>
-    )
-}
+const CampusScreen = ({navigation}) => {
 
-export default CampusScreen
+    useLayoutEffect(() => {});
+
+  const { container, map } = styles;
+  return (
+    <View style={container}>
+      <MapView 
+      showsUserLocation
+      showsMyLocationButton
+      initialRegion={INITAL_REGION}
+      style={map} />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default CampusScreen;
