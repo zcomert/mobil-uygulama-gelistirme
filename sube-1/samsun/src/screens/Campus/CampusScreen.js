41c782e0d1b9561f6441f8d3962466ca670eed89
Zafer CÖMERT <zafer.comert@hotmail.com>
2024-04-25 11:14:31 +0300

    MapView has been added.

--- sube-1/samsun/src/screens/Campus/CampusScreen.js
+++ sube-1/samsun/src/screens/Campus/CampusScreen.js
@@ -1,11 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import { INITAL_REGION } from './locations'
 
 const CampusScreen = () => {
     return (
         <View style={styles.container}>
             <MapView
+                initialRegion={INITAL_REGION}
                 provider={PROVIDER_GOOGLE}
                 showsUserLocation
                 showsMyLocationButton
