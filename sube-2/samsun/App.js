9f289c93ad0712ff1a627fc48d4a27f2e39bfa8a
zcomert <zcomert@samsun.edu.tr>
2024-05-02 11:16:39 +0300

    ContextApi has been defined.

--- sube-2/samsun/App.js
+++ sube-2/samsun/App.js
@@ -1,75 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header/Header';
-import Categories from './src/components/categories/Categories';
-import Events from './src/components/events/Events';
-import CategoryHeader from './src/components/categories/CategoryHeader';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/Home/HomeScreen'
-import CampusScreen from './src/screens/Campus/CampusScreen'
-import AcademicScreen from './src/screens/Academic/AcademicScreen'
-import AdministrativeScreen from './src/screens/Administrative/AdministrativeScreen'
-import StudentsScreen from './src/screens/Student/StudentsScreen'
-import SpeakerScreen from './src/screens/Speaker/SpeakerScreen'
-import Navigation from './src/components/navigation/Navigation';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Header from "./src/components/header/Header";
+import Categories from "./src/components/categories/Categories";
+import Events from "./src/components/events/Events";
+import CategoryHeader from "./src/components/categories/CategoryHeader";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/Home/HomeScreen";
+import CampusScreen from "./src/screens/Campus/CampusScreen";
+import AcademicScreen from "./src/screens/Academic/AcademicScreen";
+import AdministrativeScreen from "./src/screens/Administrative/AdministrativeScreen";
+import StudentsScreen from "./src/screens/Student/StudentsScreen";
+import SpeakerScreen from "./src/screens/Speaker/SpeakerScreen";
+import Navigation from "./src/components/navigation/Navigation";
+import { AppProvider } from "./src/context/AppContext";
 
 const Stack = createNativeStackNavigator();
 
-export default function App()
-{
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen"
-          component={HomeScreen}
-          options={{
-            title: 'Ana Sayfa',
-            headerShown: false
-          }}
+    <AppProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{
+              title: "Ana Sayfa",
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="CampusScreen"
+            component={CampusScreen}
+            options={{
+              title: "Yerleşkeler",
+            }}
+          />
 
-        />
-        <Stack.Screen
-          name="CampusScreen"
-          component={CampusScreen}
-          options={{
-            title: "Yerleşkeler"
-          }}
-        />
+          <Stack.Screen
+            name="AcademicScreen"
+            component={AcademicScreen}
+            options={{
+              title: "Akademik",
+            }}
+          />
 
-        <Stack.Screen
-          name="AcademicScreen"
-          component={AcademicScreen}
-          options={{
-            title: "Akademik"
-          }}
-        />
-
-        <Stack.Screen
-          name="AdministrativeScreen"
-          component={AdministrativeScreen}
-          options={{
-            title: "İdari"
-          }}
-        />
-        <Stack.Screen name="StudentScreen"
-          component={StudentsScreen}
-          options={{
-            title: "Öğrenciler"
-          }}
-        />
-        <Stack.Screen
-          name="SpeakerScreen"
-          component={SpeakerScreen}
-          options={{
-            title: "Konuşmacı"
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen
+            name="AdministrativeScreen"
+            component={AdministrativeScreen}
+            options={{
+              title: "İdari",
+            }}
+          />
+          <Stack.Screen
+            name="StudentScreen"
+            component={StudentsScreen}
+            options={{
+              title: "Öğrenciler",
+            }}
+          />
+          <Stack.Screen
+            name="SpeakerScreen"
+            component={SpeakerScreen}
+            options={{
+              title: "Konuşmacı",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppProvider>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
--- sube-2/samsun/src/screens/Speaker/SpeakerScreen.js
+++ sube-2/samsun/src/screens/Speaker/SpeakerScreen.js
@@ -1,30 +1,32 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
-import speakersdata from '../../components/events/eventdata'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import speakersdata from "../../components/events/eventdata";
 
-const SpeakerScreen = ({ route }) => {
+const SpeakerScreen = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+        headerRight : () => (<Text>hey</Text>)
+    });
+  });
 
-    const speaker = speakersdata
-        .filter(i => i.id === route?.params?.id)[0];
+  const speaker = speakersdata.filter((i) => i.id === route?.params?.id)[0];
 
-    return (
-        <View style={styles.container}>
-            <Image
-                style={styles.cover}
-                source={{ uri: speaker?.src }} />
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Image style={styles.cover} source={{ uri: speaker?.src }} />
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    cover: {
-        resizeMode: 'contain',
-        width: '100%',
-        height: '100%'
-    }
-})
-export default SpeakerScreen
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cover: {
+    resizeMode: "contain",
+    width: "100%",
+    height: "100%",
+  },
+});
+export default SpeakerScreen;
